d98d67eae76eba91c3185a7c7af5fc7f11c6bd96
JayMehta-21 <jaymehta466@gmail.com>
2026-02-22 23:51:20 +0530

    Design CRM schema wuth clients and interaction table

---
+++ CRM_system/database.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+file_name="client_database.db"
+
+def get_connect():
+    return sqlite3.connect(file_name)
+
+def create_table():
+
+    conn= get_connect()
+    cursor=conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS clients(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   name TEXT NOT NULL,
+                   phone_no TEXT NOT NULL,
+                   email TEXT NOT NULL UNIQUE,
+                   status TEXT NOT NULL CHECK(status IN("lead","contacted","converted","lost")),
+                   created_at TEXT DEFAULT CURRENT_TIMESTAMP
+                   )
+""")
+    
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS interactions(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   client_id INTEGRE NOT NULL,
+                   note TEXT NOT NULL,
+                   interaction_type TEXT NOT NULL CHECK(interaction_type IN("call","email","meeting")),
+                   created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+                   FOREIGN KEY (client_id) REFERENCES clients(id)
+                   )
+""")
+    
+    conn.commit()
+    conn.close()
