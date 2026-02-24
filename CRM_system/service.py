d1ce308829ff9fc9379bb9d27a038ca8545dbad7
JayMehta-21 <jaymehta466@gmail.com>
2026-02-24 23:54:02 +0530

    add client interaction workflow with validations

--- CRM_system/service.py
+++ CRM_system/service.py
@@ -1,7 +1,9 @@
 import sqlite3
 
 from database import(
-    add_client
+    add_client,
+    get_client_by_id,
+    interaction
 )
 
 def create_client(name,phone,email):
@@ -17,3 +19,41 @@ def create_client(name,phone,email):
         add_client(name,phone,email)
     except sqlite3.IntegrityError:
         raise ValueError("Client already exists")
+
+def add_interaction(client_id,type,note):
+    
+    client_detail= get_client_by_id(client_id)
+
+    if client_detail is None:
+        raise ValueError("Client not found")
+    
+    status= client_detail[4]
+
+    valid_interacion={"call","email","meeting"}
+
+    if type not in valid_interacion:
+        raise ValueError("Invalid interaction type")
+
+    try :
+        interaction(client_id,type,note)
+    except sqlite3.IntegrityError:
+        raise ValueError("Check Contraints Failed")
+
+def update_client_status(client_id,new_status):
+
+    client_details= get_client_by_id(client_id)
+
+    if client_details is None:
+        raise ValueError("Client not Exists")
+    
+    current_status=client_details[4]
+
+    valid_transactions={
+        "lead":["contacted","converted"],
+        "contacted": ["converted","lost"],
+        "converted": [],
+        "lost":[],
+    }
+
+    if new_status not in valid_transactions(current_status):
+        raise ValueError("Invalid status transitions")
