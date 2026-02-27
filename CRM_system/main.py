a1d17e7a412b406da8c1b60f9c2db3e39f8ad0b7
JayMehta-21 <jaymehta466@gmail.com>
2026-02-27 23:42:54 +0530

    resolve f-string bug and improve input validation

--- CRM_system/main.py
+++ CRM_system/main.py
@@ -2,10 +2,10 @@ from database import (
     create_table
 )
 
-from service import(
-    update_client_status,
+from service import (
     create_client,
     add_interaction,
+    update_client_status,
     view_records
 )
 
@@ -15,71 +15,80 @@ def main():
 
     while True:
 
-        print(".......CRM System.......")
+        print("---------------- CRM System ----------------")
         print("""
-    1️⃣. Add client 
-    2️⃣. Add Interaction
+    1️⃣. Add client
+    2️⃣. Add interaction
     3️⃣. Update Status
-    4️⃣. View Records
+    4️⃣. View Records By Status
     5️⃣. Exit
     """)
         
-        choice=input("Choose from menu : ")
+        choice  = input("Choose from menu : ")
 
-        if choice=="1":
-            name=input("Enter the name of client :")
-            phone=input("Client phone no : ")
-            email=input("Client email :")
+        if choice == "1":
 
-            try :
-                create_client(name,phone,email)
-                print("Client added Successfully")
-            except ValueError as e:
-                print(f"Error : {e}")
-            except Exception as e:
-                print(f"Error : {e}")
-        elif choice=="2":
-
-            client_id=int(input("Enter the Client Id :"))
-            type=input("Enter the way of Interaction :")
-            note=input("Leave some note :")
-
-            try :
-                add_interaction(client_id,type,note)
-            except ValueError as e:
-                print(f"Error : {e}")
-            except Exception as e:
-                print(f"Error : {e}")
-
-        elif choice=="3":
-            client_id=int(input("Enter the Client Id :"))
-            new_status=input("Enter the Updated Status : ")
+            name  = input("Enter the name of client: ")
+            phone = input("Clients phone_no : ")
+            email = input("Clients email : ")
 
             try:
-                update_client_status(client_id,new_status)
-            except ValueError as e :
+                create_client(name, phone, email)
+                print("Client added successfully✔️")
+            except ValueError as e:
                 print(f"Error : {e}")
             except Exception as e:
                 print(f"Error : {e}")
-                
-        elif choice=="4":
-            
-            status=input("Enter the Status: ")
 
-            try :
+
+        elif choice == "2":
+            
+            client_id = int(input("Enter the client_id : "))
+            interaction_type = input("Enter the way of interaction: ")
+            note = input("Leave some note : ")
+
+            try:
+                add_interaction(client_id, interaction_type, note)
+                print("Interaction Record saved successfully✔️")
+            except ValueError as e:
+                print(f"Error : {e}")
+            except Exception as e:
+                print(f"Error : {e}")
+
+
+        elif choice == "3":
+            
+            client_id = int(input("Enter the client_id : "))
+            new_status = input("Enter the updated status (contacted/ converted/ lost): ")
+
+            try:
+                update_status = update_client_status(client_id, new_status)
+                print(f"Client_id : {update_status['client_id']}")
+                print(f"New status : {update_status['Update_status']}")
+            except ValueError as e:
+                print(f"Error : {e}")
+            except Exception as e:
+                print(f"Error : {e}")
+
+
+        elif choice == "4":
+            
+            status = input("Enter the Status: ")
+
+            try:
                 records = view_records(status)
                 for view in records:
-                    print(f"{view[0]}. {view[1]} |  {view[2]} | {view[3]} | {view[4]} | {view[5]}")
-            except ValueError as e :
-                print(f"Error : {e}")
-            except Exception as e:
+                    print(f"{view[0]}. {view[1]} | {view[2]} | {view[3]} | {view[4]} | {view[5]}")
+            except ValueError as e:
                 print(f"Error : {e}")
 
-        elif choice=="5":
-            print("Goodbye!")
+
+        elif choice == "5":
+            print("Good-Bye.....")
             break
-        else :
-            print("Choose from Menu only")
 
-if __name__ =="__main__":
+        else:
+            print("Choose from menu only")
+
+if __name__ == "__main__":
     main()
