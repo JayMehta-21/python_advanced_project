abf6b7bc5cee13f930aee32218893788a9dca77c
JayMehta-21 <jaymehta466@gmail.com>
2026-02-27 23:42:17 +0530

    Enforce FK cascade and enable foreign key constraints

--- CRM_system/database.py
+++ CRM_system/database.py
@@ -1,98 +1,100 @@
 import sqlite3
 
-file_name="client_database.db"
+file_name = "client_database.db"
 
 def get_connect():
-    return sqlite3.connect(file_name)
+    conn =  sqlite3.connect(file_name)
+    conn.execute("PRAGMA foreign_keys = ON")
+    return conn
 
 def create_table():
 
-    conn= get_connect()
-    cursor=conn.cursor()
+    conn = get_connect()
+    cursor = conn.cursor()
 
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS clients(
+        CREATE TABLE IF NOT EXISTS clients (
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                    name TEXT NOT NULL,
                    phone_no TEXT NOT NULL,
                    email TEXT NOT NULL UNIQUE,
-                   status TEXT NOT NULL CHECK(status IN("lead","contacted","converted","lost")),
+                   status TEXT NOT NULL CHECK(status IN("lead", "contacted", "converted", "lost")),
                    created_at TEXT DEFAULT CURRENT_TIMESTAMP
                    )
 """)
     
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS interactions(
+        CREATE TABLE IF NOT EXISTS interactions (
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   client_id INTEGRE NOT NULL,
+                   client_id INTEGER NOT NULL,
                    note TEXT NOT NULL,
-                   interaction_type TEXT NOT NULL CHECK(interaction_type IN("call","email","meeting")),
+                   interaction_type TEXT NOT NULL CHECK(interaction_type IN("call", "email", "meeting")),
                    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-                   FOREIGN KEY (client_id) REFERENCES clients(id)
+                   FOREIGN KEY (client_id) REFERENCES clients(id) ON DELETE CASCADE
                    )
 """)
     
     conn.commit()
     conn.close()
 
-def add_client(name,phone,email):
+def add_client(name, phone, email):
     
-    conn= get_connect()
-    cursor= conn.cursor()
+    conn = get_connect()
+    cursor = conn.cursor()
 
     cursor.execute("""
-        INSERT INTO clients (name,phone_no,email,status) VALUES (?,?,?,?)
-""",(name,phone,email,"lead"))
+        INSERT INTO clients (name, phone_no, email, status) VALUES (?, ?, ?, ?)    
+""", (name, phone, email, "lead"))
     
     conn.commit()
     conn.close()
 
 def get_client_by_id(client_id):
 
-    conn=get_connect()
+    conn = get_connect()
     cursor = conn.cursor()
 
-    cursor.execute(" SELECT * FROM clients WEHERE client_id =?",(client_id,) )
+    cursor.execute("SELECT * FROM clients WHERE id = ?",(client_id, ))
 
     row = cursor.fetchone()
     conn.close()
 
     return row
 
-def interaction(client_id,type, note):
+def interaction(client_id, interaction_type, note):
 
-    conn=get_connect()
-    cursor=conn.cursor()
+    conn = get_connect()
+    cursor = conn.cursor()
 
     cursor.execute("""
-        INSERT INTO interactions (client_id,note,interaction_type)
-                   VALUES(?,?,?)
-""",(client_id,note,type))
+        INSERT INTO interactions (client_id, note, interaction_type)
+                   VALUES (?, ?, ?)
+""",(client_id, note, interaction_type))
+    
+    conn.commit()
+    conn.close()
+
+def update_client(client_id, new_status):
+
+    conn = get_connect()
+    cursor = conn.cursor()
+
+    cursor.execute("UPDATE clients SET status = ? WHERE id = ?", (new_status, client_id))
 
     conn.commit()
     conn.close()
 
-def update_client(client_id,new_status):
+def view_records_by_status(status):
 
-    conn=get_connect()   
-    cursor=conn.cursor()
-
-    cursor.execute("UPDAT clients SET status = ? WEHERE id = ?",(new_status,client_id))
-
-    conn.commit()
-    conn.close()
-
-def view_record_by_status(status):
-
-    conn=get_connect()
-    cursor=conn.cursor()
+    conn = get_connect()
+    cursor = conn.cursor()
 
     cursor.execute("""
-        SELECT * FROM clients WEHERE status = ?
-"""(status,))
-
-    row=cursor.fetchall()
+        SELECT * FROM clients WHERE status = ? 
+""",(status, ))
+    
+    row = cursor.fetchall()
 
     conn.close()
 
-    return row 
+    return row
